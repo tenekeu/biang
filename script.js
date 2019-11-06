31318cee11447a9979ef5e30dc9a89d87d0106f0
yvan tenekeu <ytenekeu@gmail.com>
2019-11-06 19:45:02 +0100

    alternance de couleur intervalle de 2 secondes

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-var tab=['red','blue','white','black','gris'];
+var k=2;
 function entierAleatoire(min, max)
 {
  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 setInterval(function(){
-var q=entierAleatoire(0,4);
-document.getElementById("body").style.background-color = tab[q];
- }, 3000);
+	var q=entierAleatoire(0,4);
+	while(q===k){
+		q=entierAleatoire(0,4);
+	}	
+	body=document.getElementById("body");
+	switch(q){
+		case(0):{
+			body.style.background = "red";
+		}
+		break;
+		case(1):{
+			body.style.background = "blue";
+		}
+		break;
+		case(2):{
+			body.style.background = "white";
+		}
+		break;
+		case(3):{
+			body.style.background = "black";
+		}
+		break;
+		case(4):{
+			body.style.background = "gris";
+		}
+		break;
+	}	
+	
+ }, 2000);
